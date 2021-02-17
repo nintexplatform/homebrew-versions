21afbce58e42147192a3db1606efd32a309a4be6
Vijendra kumar <vijendra.kumar@nintex.com>
2021-02-17 14:05:20 +1100

    added kubectl version from 1.18.8 to 1.20.2

---
+++ kubernetes-cli@1.18.8.rb
@@ -0,0 +1,55 @@
+class KubernetesCliAT1188 < Formula
+    desc "Kubernetes command-line interface"
+    homepage "https://kubernetes.io/"
+    url "https://github.com/kubernetes/kubernetes.git",
+        tag:      "v1.18.8",
+        revision: "9f2892aab98fe339f3bd70e3c470144299398ace"
+    license "Apache-2.0"
+    head "https://github.com/kubernetes/kubernetes.git"
+  
+    bottle do
+      cellar :any_skip_relocation
+      sha256 "6f93ab2c3883f480dbdc0e45e88f3e558ab0c9ffaeb58e906d69a39a4d381f27" => :catalina
+      sha256 "cd1c01158cf241b07f0e4875acf151099fabf44b5c55e7b33b457a9eb0fefcfe" => :mojave
+      sha256 "a86b0894d94dbb51d75efec1dc2781cd83d657a9c7ad2bb65b7ad24971339f0d" => :high_sierra
+    end
+  
+    depends_on "go" => :build
+  
+    uses_from_macos "rsync" => :build
+  
+    def install
+      # Don't dirty the git tree
+      rm_rf ".brew_home"
+  
+      # Make binary
+      system "make", "WHAT=cmd/kubectl"
+      bin.install "_output/bin/kubectl"
+  
+      # Install bash completion
+      output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "bash")
+      (bash_completion/"kubectl").write output
+  
+      # Install zsh completion
+      output = Utils.safe_popen_read("#{bin}/kubectl", "completion", "zsh")
+      (zsh_completion/"_kubectl").write output
+  
+      # Install man pages
+      # Leave this step for the end as this dirties the git tree
+      system "hack/generate-docs.sh"
+      man1.install Dir["docs/man/man1/*.1"]
+    end
+  
+    test do
+      run_output = shell_output("#{bin}/kubectl 2>&1")
+      assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+  
+      version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+      assert_match "GitTreeState:\"clean\"", version_output
+      if build.stable?
+        assert_match stable.instance_variable_get(:@resource)
+                           .instance_variable_get(:@specs)[:revision],
+                     version_output
+      end
+    end
+  end
